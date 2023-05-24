f4157272f750d9e80d5b173b7a2b528de774b0c0
BrunaMello <brunacrespomello@gmail.com>
2023-05-24 16:46:14 +0100

    Initial Commit

---
+++ mell Scrumdinger/mell_ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  mell_ScrumdingerApp.swift
+//  mell Scrumdinger
+//
+//  Created by Bruna Mello on 24/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct mell_ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
